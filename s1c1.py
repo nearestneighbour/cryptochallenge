78116d0a22ce1757458924fc409c6912764cd01e
Tido Bergmans <tido_berg@hotmail.com>
2019-06-03 11:50:45 +0200

    updated to challenge 5

--- s1c1.py
+++ s1c1.py
@@ -1,6 +1,10 @@
 from base64 import b64encode
 
 def hex2b64(h):
+    """
+    Bit of a cheat since it uses b64encode
+    """
+    
     if type(h) == int:
         h = hex(h)
     return b64encode(bytes.fromhex(h))
--- s1c2.py
+++ s1c2.py
@@ -1,9 +1,6 @@
-def hex_xor(a, b, s=False):
-    if type(a) == str:
-        a = int(a,16)
-    if type(b) == str:
-        b = int(b,16)
-    if s:
-        return hex(a ^ b)
-    else:
-        return a ^ b
+def byte_xor(input, char):
+    """
+    Single-byte XOR encrypt a message.
+    """
+
+    return bytes([byte^char for byte in input])
