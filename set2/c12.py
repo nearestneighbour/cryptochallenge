805bd0cb1cff24f0b1b4a280fd4c63826ac3dfee
nearestneighbour <tido_berg@hotmail.com>
2019-06-18 23:22:10 +0200

    added comment

--- set2/c12.py
+++ set2/c12.py
@@ -27,6 +27,7 @@ def find_string(data, cipher, bsz):
 
 if __name__ == '__main__':
     # This part should be unknown, i.e. we shouldn't know how the encryption works
+    # We don't know the key and cipher function (but we can call it)
     unknown_str = pad_bytes(b64decode("""Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
     aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
     dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
