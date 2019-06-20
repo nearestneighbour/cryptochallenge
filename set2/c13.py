7db4c174f515ce15fd62c4be74d6d525657c8f70
nearestneighbour <tido_berg@hotmail.com>
2019-06-20 23:21:39 +0200

    started working on 13

--- set2/c13.py
+++ set2/c13.py
@@ -1,23 +1,27 @@
 import numpy as np
-from c9 import encrypt_ecb, decrypt_ecb
+from c9 import ecb_cipher
 from c12 import find_blocksize
 
-def parse_profile(url):
-    return {prop.split('=')[0]: prop.split('=')[1] for prop in url.split('&')}
+class profile_manager:
+    def __init__(self, key=None):
+        self.cph = ecb_cipher(key if key else np.random.bytes(16))
 
-def profile_for(email):
-    assert ('&' not in email and '=' not in email), "Stupid hacker"
-    profile = 'email=' + email + 'uid=10&role=user'
-    return encrypt_ecb(profile.encode(), kklklm) # key is a global variable
+    def parse_profile(self, url):
+        return {prop.split('=')[0]: prop.split('=')[1] for prop in url.split('&')}
 
-def decrypt_profile(profile, key):
-    return parse_profile(decrypt_ecb(profile, key))  # key is a global variable
+    def decrypt_profile(profile):
+        return self.parse_profile(self.cph.decrypt(profile))
 
-if __name__ == '__main__':
-    # We don't know the key or the block size
-    key = np.random.bytes(16)
-    # We can only know the output of the profile_for function for a given input
-    myprofile = profile_for('')
-    bsz = find_blocksize(myprofile, profile_for)
+    def profile_for(self, addr):
+        assert ('&' not in addr and '=' not in addr), "Stupid hacker"
+        profile = 'email=' + addr + 'uid=10&role=user'
+        return self.cph.encrypt(profile.encode())
+
+def main():
+    # We don't know the key, block size or encryption function
+    # Only thing we know is the output of profile_for for a given input
+    prf_manager = profile_manager()
+    myprofile = prf_manager.profile_for('')
+    bsz = find_blocksize(myprofile, prf_manager.profile_for)
     # Mail should be of such length that 'user' is at the start of a new block
     mail_length = (len(myprofile) - 5) % bsz
