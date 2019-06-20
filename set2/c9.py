a926f6337a431b706b1a3e2a30b0f740a633c6af
Tido Bergmans <tido_berg@hotmail.com>
2019-06-20 21:14:46 +0200

    added some more functionality

--- set2/c9.py
+++ set2/c9.py
@@ -1,3 +1,5 @@
+from Crypto.Cipher import AES
+
 def pad_bytes(msg, bsz):
     if type(msg) == str:
         msg = bytes([ord(i) for i in msg])
@@ -6,6 +8,18 @@ def pad_bytes(msg, bsz):
     for i in range(npad):
         msg += bytes([npad])
     return msg
+
+def unpad_bytes(msg):
+    return msg[:-msg[-1]]
+
+# Updated version of the functions from challenge 7, convenient for future challenges
+def encrypt_ecb(data, key):
+    cph = AES.new(key, AES.MODE_ECB)
+    return cph.encrypt(pad_bytes(data, len(key)))
+
+def decrypt_ecb(data, key):
+    cph = AES.new(key, AES.MODE_EDB)
+    return unpad_bytes(cph.decrypt(data))
 
 if __name__ == '__main__':
     msg = 'YELLOW SUBMARINE'
