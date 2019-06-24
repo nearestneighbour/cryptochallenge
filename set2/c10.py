f2d048f389e1d5d63bf146593ace17a6525e7097
nearestneighbour <tido_berg@hotmail.com>
2019-06-24 17:32:13 +0200

    minor update

--- set2/c10.py
+++ set2/c10.py
@@ -8,17 +8,25 @@ import numpy as np
 from c09 import ecb_cipher, pad_pkcs7, unpad_pkcs7
 
 class cbc_cipher(ecb_cipher):
-    def encrypt(self, data, iv):
+    # Set iv to None to generate a random IV
+    # Set iv to -1 to generate a random IV for every encryption call
+    def __init__(self, key=None, iv=None):
+        super().__init__(key)
+        self.iv = iv if iv else np.random.bytes(self.bsz)
+
+    def encrypt(self, data):
         data = pad_pkcs7(data, self.bsz)
         output = bytes()
+        iv = self.iv
         for i in range(0, len(data), self.bsz):
             block = self.cph.encrypt(bytes([a^b for a,b in zip(data[i:i+self.bsz],iv)]))
             output += block
             iv = block
         return output
 
-    def decrypt(self, data, iv):
+    def decrypt(self, data):
         output = bytes()
+        iv = self.iv
         for i in range(0, len(data), self.bsz):
             block = self.cph.decrypt(data[i:i+self.bsz])
             output += bytes([a^b for a,b in zip(block,iv)])
@@ -27,7 +35,11 @@ class cbc_cipher(ecb_cipher):
 
     @staticmethod
     def encrypt_cbc(data, key=np.random.bytes(16), iv=np.random.bytes(16)):
-        return cbc_cipher(key).encrypt(data, iv)
+        return cbc_cipher(key, iv).encrypt(data)
+
+    @staticmethod
+    def decrypt_cbc(data, key, iv):
+        return cbc_cipher(key, iv).decrypt(data)
 
 def main():
     from base64 import b64decode
