2c67f3cfc621896a1d4f5a90acd2197766d7cc4b
nearestneighbour <tido_berg@hotmail.com>
2019-06-24 22:51:08 +0200

    finished c17

--- set3/c17.py
+++ set3/c17.py
@@ -1,10 +1,11 @@
 import numpy as np
 from c10 import cbc_cipher
+from c15 import unpad_pkcs7
 
 class random_cipher(cbc_cipher):
-    def __init__(self, key, iv):
+    def __init__(self, key=None, iv=None):
         super().__init__(key, iv)
-        with open('c17data') as f:
+        with open('set3/c17data') as f:
             self.strings = f.read().split('\n')[:-1]
 
     def encrypt_string(self):
@@ -12,8 +13,29 @@ class random_cipher(cbc_cipher):
         return self.encrypt(string)
 
     def decrypt_string(self, data):
-        string = self.decrypt(string)
+        string = self.decrypt(data)
         return string != None
 
-# All strings are different length and thus different padding.
-# Therefore we can identify strings by their last byte.
+# With help from: https://thmsdnnr.com/tutorials/javascript/cryptopals/2017/10/02/cryptopals-set3-challenge-17-cbc-padding-oracle.html
+def decode_block_cbc(block, prev):
+    guess = [0 for i in range(16)]
+    for i in range(16):
+        b = [guess[15-k]^(i+1) for k in range(i-1,-1,-1)]
+        for j in range(256):
+            guess[15-i] = j^prev[15-i]
+            synth = (15-i) * b'X' + bytes([guess[15-i]^(i+1)]) + bytes(b)
+            if cph.decrypt_string(synth + block):
+                break
+    return bytes([guess[i]^prev[i] for i in range(16)])
+
+iv = np.random.bytes(16)
+cph = random_cipher(iv=iv)
+
+def main():
+    ct = cph.encrypt_string()
+    # Decode output block by block
+    blocks = [decode_block_cbc(ct[:16], iv)]
+    for i in range(len(ct) // 16 - 1):
+        blocks += [decode_block_cbc(ct[i*16+16:i*16+32], ct[i*16:i*16+16])]
+    pt = unpad_pkcs7(b''.join(blocks))
+    print(pt)
