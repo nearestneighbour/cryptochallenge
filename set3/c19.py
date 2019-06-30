9347fb3186e652897235348d25996c3c07b77373
nearestneighbour <tido_berg@hotmail.com>
2019-06-30 21:28:40 +0200

    finished c19 (kinda)

--- set3/c19.py
+++ set3/c19.py
@@ -1,30 +1,24 @@
-import numpy as np
-
-from c06 import find_keysize, decrypt_repeat_xor
+from c03 import decrypt_single_xor
 from c18 import ctr_cipher
 
-from base64 import b64decode
-with open('set3/c19data') as f:
-    data = [b64decode(line.strip()) for line in f.readlines()]
+def main():
+    from base64 import b64decode
+    cph = ctr_cipher(prefix=bytes(8), little_endian=True)
+    with open('set3/c19data') as f:
+        data = [b64decode(line.strip()) for line in f.readlines()]
+        data = [cph.encrypt(line) for line in data]
 
-cph = ctr_cipher(prefix=bytes(8))
-data_enc = [cph.encrypt(line) for line in data]
+    # Since all messages have the same key, each line[i] of the lines in the data is
+    # encoded with the same character key[i]. So create a block of all line[i] and
+    # decrypt it using single byte XOR decryption from challenge 3. This works great
+    # except for the few longest lines, because here the sample size is too low.
+    m = max([len(line) for line in data])
+    key = b''
+    for i in range(m):
+         block = b''.join([bytes([line[i]]) for line in data if len(line) > i])
+         _, char = decrypt_single_xor(block)
+         key += bytes([char])
 
-# Pad lines so they have equal length and still same keystream
-m = max([len(line) for line in data_enc])
-add = [m-len(line) for line in data_enc]
-maxline = np.argmin(add)
-data_padded = [line+data_enc[maxline][-idx:] for line,idx in zip(data_enc,add)]
-data_padded[maxline] = data_padded[maxline][:m]
-
-data_string = b''.join([line for line in data_padded])
-txt, _ = decrypt_repeat_xor(data_string, m)
-
-#dist = np.zeros(n-1)
-#for i in range(2,n+1):
-#    blocks = b''.join([line[:n] for line in data if len(line)])
-#    dist[i-2], _ = find_keysize(blocks, [i])
-#ksz = np.argmin(dist) + 2
-
-
-#txt, key = decrypt_repeat_xor(blockstr, len(data_trunc[0]))
+    for i in range(len(data)):
+        line = bytes([data[i][j]^key[j] for j in range(len(data[i]))])
+        print(line)
