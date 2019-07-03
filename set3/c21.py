cb0027784ec279e6c38bcaef886fa3304312d237
Tido Bergmans <tido_berg@hotmail.com>
2019-07-03 15:11:58 +0200

    finished c21

--- set3/c21.py
+++ set3/c21.py
@@ -1,39 +1,73 @@
-import numpy as np
-
 # Implementation of the Mersenne Twister RNG based on pseudocode from:
 # https://en.wikipedia.org/wiki/Mersenne_Twister
+class mt19973_rng:
+    def __init__(self, seed, **kwargs):
+        # Parameters according to MT19937:
+        self.w = kwargs.pop('w', 32)
+        self.n = kwargs.pop('n', 624)
+        self.m = kwargs.pop('m', 397)
+        self.r = kwargs.pop('r', 31)
+        self.a = kwargs.pop('a', 0x9908B0DF)
+        self.u = kwargs.pop('u', 11)
+        self.d = kwargs.pop('d', 0xFFFFFFFF)
+        self.s = kwargs.pop('s', 7)
+        self.b = kwargs.pop('b', 0x9D2C5680)
+        self.t = kwargs.pop('t', 15)
+        self.c = kwargs.pop('c', 0xEFC60000)
+        self.l = kwargs.pop('l', 18)
+        self.f = kwargs.pop('f', 0x6C078965)
+        self.initialize(seed)
 
-# Parameters according to MT19937-64:
-w = 64 # word size (in bits)
-n = 312 # degree of recurrence
-m = 156 # middle word, offset used in recurrence relation defining x. 1 ≤ m < n
-r = 31 # sep. point of a word, or #bits of the lower bitmask. 0 ≤ r ≤ w - 1
-a = 0xB5026F5AA96619E9 # coefficients of the rational normal form twist matrix
-# Additional parameters for tempering bitmasks/bit shifts:
-u = 29
-d = 0x5555555555555555
-s = 17
-b = 0x71D67FFFEDA60000
-t = 37
-c = 0xFFF7EEE000000000
-l = 43
+    def initialize(self, seed):
+        self.mt = self.n * [0] # generator state
+        self.lower = (1 << self.r) - 1
+        self.upper = self.low_bits((1 << self.w) - 1 - self.lower)
+        self.index = self.n
+        self.mt[0] = seed
+        for i in range(self.n-1):
+            tmp = self.mt[i] ^ (self.mt[i] >> (self.w-2))
+            self.mt[i+1] = self.low_bits(self.f*tmp + i + 1)
 
-def bitwise_not(n):
-    return (1 << w) - 1 - n
+    def rand(self):
+        if self.index == self.n:
+            self.twist()
+        y = self.mt[self.index]
+        y ^= (y >> self.u) & self.d
+        y ^= (y << self.s) & self.b
+        y ^= (y << self.t) & self.c
+        y ^= y >> self.l
+        self.index += 1
+        return self.low_bits(y)
 
-def low_bits(n):
-    return n - ((n >> w) << w)
+    def twist(self):
+        for i in range(self.n-1):
+            x = (self.mt[i] & self.upper) + (self.mt[i+1] & self.lower)
+            x = (x >> 1) if (x % 2) == 0 else (x >> 1) ^ self.a
+            self.mt[i] = self.mt[(i+self.m) % self.n] ^ x
+        self.index = 0
 
-mt = np.zeros(n, dtype='int64') # to store generator state
-lower_mask = (1 << r) - 1
-tmp = bitwise_not(lower_mask, 64)
-upper_mask = low_bits(tmp, w)
+    def low_bits(self, i):
+        return i & (2**self.w-1)
 
-def seed_mt(seed):
-    index = n
-    mt[0] = seed
-    for i in range(n-1):
-        tmp = mt[i] ^ (mt[i] >> (w-2))
-        mt[i+1] = lowest_bits(f*tmp + i-1) # what is f???
+class mt19973_64_rng(mt19973_rng):
+    def __init__(self, seed, **kwargs):
+        # Parameters according to MT19937-64:
+        self.w = kwargs.pop('w', 64)
+        self.n = kwargs.pop('n', 312)
+        self.m = kwargs.pop('m', 156)
+        self.r = kwargs.pop('r', 31)
+        self.a = kwargs.pop('a', 0xB5026F5AA96619E9)
+        self.u = kwargs.pop('u', 29)
+        self.d = kwargs.pop('d', 0x5555555555555555)
+        self.s = kwargs.pop('s', 17)
+        self.b = kwargs.pop('b', 0x71D67FFFEDA60000)
+        self.t = kwargs.pop('t', 37)
+        self.c = kwargs.pop('c', 0xFFF7EEE000000000)
+        self.l = kwargs.pop('l', 43)
+        self.f = kwargs.pop('f', 6364136223846793005)
+        self.initialize(seed)
 
-def extract_number()
+def main():
+    rng = mt19973_rng(seed=5489)
+    for i in range(10):
+        print(rng.rand())
