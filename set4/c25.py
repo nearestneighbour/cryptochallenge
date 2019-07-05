d33083bc11925e94144eab31e139a1727f7f0274
Tido Bergmans <tido_berg@hotmail.com>
2019-07-05 13:15:35 +0200

    finished c25

--- set4/c25.py
+++ set4/c25.py
@@ -1,13 +1,22 @@
 from c10 import cbc_cipher
 from c18 import ctr_cipher
 
-from base64 import b64decode
-# Load data from the ECB exercise
-with open('set2/c10data') as f:
-    data = b64decode(f.read())
-pt = cbc_cipher(b'YELLOW SUBMARINE', bytes(16)).decrypt(data)
+class ctr_cipher_edit(ctr_cipher):
+    def edit(self, ct, offset, newtxt):
+        pt = self.decrypt(ct)
+        newpt = pt[:offset] + newtxt + pt[offset+len(newtxt):]
+        return self.encrypt(newpt)
 
-ctr = ctr_cipher()
+def main():
+    from base64 import b64decode
+    cph = ctr_cipher_edit()
+    # Load data from the ECB exercise and encrypt it. For the attack we only have
+    # access to the ciphertext and the edit function, not the encryption function.
+    with open('set2/c10data') as f:
+        data = b64decode(f.read())
+        ct = cph.encrypt(cbc_cipher(b'YELLOW SUBMARINE', bytes(16)).decrypt(data))
 
-def edit(ct, key, offset, newtxt):
-    pass
+    # Reveal keystream by replacing the entire ciphertext with 0-bytes
+    keystream = cph.edit(ct, 0, bytes(len(ct)))
+    # XOR ciphertext against keystream to reveal plaintext
+    print(bytes([a^b for a,b in zip(ct, keystream)]))
