d3c65d05b55af95131ec676e3e3d376a9ee61e4a
nearestneighbour <tido_berg@hotmail.com>
2019-07-07 19:19:15 +0200

    finished c31

--- set4/c31_attack.py
+++ set4/c31_attack.py
@@ -1,33 +1,34 @@
 import requests as r
 from time import time
-import numpy as np
 
-filename = 'foo'
-emptyurl = 'http://localhost:5000/test'
-url = emptyurl + '?file=' + filename + '&signature='
+def attack(emptyurl, filename):
+    url = emptyurl + '?file=' + filename + '&signature='
+    sig = b''
+    for i in range(20):
+        for j in range(256):
+            testsig = sig + bytes([j])
+            st = time()
+            r.get(url +  testsig.hex())
+            t = time() - st
+            if t > (len(sig) + 1) * 0.05:
+                sig += bytes([j])
+                break
+        print('Discovered {} out of 20 bytes, signature so far: {}'.format(i, sig.hex()))
+    return sig
 
-# Discover baseline connection time
-t = []
-for i in range(10):
-    st = time()
-    r.get(emptyurl)
-    t += [time() - st]
-print('Baseline request duration: ', np.mean(t))
-print('Standard deviation: ', np.std(t))
-
-# Requests should take 50ms longer for every signature byte that is correct.
-# Request duration standard deviation is pretty low, so it should be easy to
-# figure out how many bytes are correct from the request duration alone without
-# having to resort to averaging multiple rounds etc.
-
-sig = b''
-for i in range(20):
-    for j in range(256):
-        testsig = sig + bytes([j])
+def main():
+    import numpy as np
+    emptyurl = 'http://localhost:5000/test'
+    # Discover baseline connection time
+    t = []
+    for i in range(10):
         st = time()
-        r.get(url +  testsig.hex())
-        t = time() - st
-        if t > (len(sig) + 1) * 0.05:
-            sig += bytes([j])
-            break
-    print('Discovered {} out of 20 bytes, signature so far: {}'.format(i, sig.hex()))
+        r.get(emptyurl)
+        t += [time() - st]
+    print('Baseline request duration: ', np.mean(t))
+    print('Standard deviation: ', np.std(t))
+    # Requests should take 50ms longer for every signature byte that is correct.
+    # Request duration standard deviation is pretty low, so it should be easy to
+    # figure out how many bytes are correct from the request duration alone
+    # without having to resort to averaging multiple rounds etc.
+    attack(emptyurl, 'foo')
--- set4/c31_server.py
+++ set4/c31_server.py
@@ -4,7 +4,7 @@ from time import sleep
 import numpy as np
 from c28 import sha1
 
-key = bytes(16)#np.random.bytes(16)
+key = np.random.bytes(16)
 app = Flask(__name__)
 
 def insecure_compare(sig1, sig2):
