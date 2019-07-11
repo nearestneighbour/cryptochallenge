a422b4886bec2a6c92bbf140702aff8768c3b0ce
nearestneighbour <tido_berg@hotmail.com>
2019-07-11 18:21:45 +0200

    finished c37

---
+++ set5/c37_attack.py
@@ -0,0 +1,24 @@
+# reuse server from challenge 31, run with command:
+# FLASK_APP=c36_server.py flask run
+
+# When the server receives a PK that is 0, the session key will also be 0. If
+# PK=N or PK=N**2, it will also be 0.
+
+import requests
+from c36_sha256 import sha256
+
+def main(pk_value): # pk_value can be e.g. 0, N or N**2
+    pk_value = hex(pk_value)[2:]
+    if len(pk_value) < 2:
+        pk_value = '0' + pk_value
+
+    url = 'http://localhost:5000/test'
+    resp = requests.get(url + '?pk=' + pk_value)
+    pk = int(resp.json()['pk'], 16)
+    salt = bytes.fromhex(resp.json()['salt'])
+
+    key = sha256().digest(b'\x00')
+    hmac = sha256().hmac(salt, key)
+
+    resp = requests.get(url + '?hmac=' + hmac.hex())
+    print(resp.text)
