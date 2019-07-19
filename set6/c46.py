967676083775e49105fbe78f5e2743899a6905ee
nearestneighbour <tido_berg@hotmail.com>
2019-07-19 18:03:21 +0200

    finished c46

--- set6/c46.py
+++ set6/c46.py
@@ -1,12 +1,23 @@
 from base64 import b64decode
 from c39 import rsa
 
-class oracle(rsa):
-    def even_odd(self, ct): # True if even
+class parity_oracle(rsa):
+    def is_even(self, ct): # True if even
         return self.decrypt(ct) % 2 == 0
 
+# This binary search algo can never find the last byte, not sure why
 def main():
-    r = oracle()
-    data = b64decode('VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFyb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ==')
-    ct = r.encrypt(data)
-    # attack
+    r = parity_oracle()
+    ct = r.encrypt(b64decode('VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGF'
+        'yb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ=='))
+    e, n = r.publickey()
+    
+    lb, ub = 0, n
+    while lb <= ub:
+        mid = (ub + lb) // 2
+        ct *= pow(2, e, n)
+        if r.is_even(ct):
+            ub = mid - 1
+        else:
+            lb = mid + 1
+        print(ub.to_bytes((ub.bit_length() + 7) // 8, 'big'))
